fc2fdf21a341d07a674b237ee375f2daea0b8df5
unknown <mark.mazzitello@VRAD-23869.VirtualRad.local>
2015-04-07 20:18:12 -0500

    Installed bower, jquery, angular, bootstrap, etc.

---
+++ grails-app/scripts/_Events.groovy
@@ -0,0 +1,29 @@
+eventCompileStart = { kind ->
+    executeNpmInstall()
+    executeGruntTasks()
+}
+
+private void executeNpmInstall() {
+    def npmInstall = "npm install"
+    println "| npm install..."
+    def proc = npmInstall.execute()
+    proc.waitFor()
+    if (proc.exitValue() != 0) {
+        println "Error installing npm dependencies"
+        println "Output: ${proc.in.text}"
+    }
+}
+
+private void executeGruntTasks() {
+    def gruntScript = "node_modules/.bin/grunt"
+    println "| Load js dependencies from cache..."
+    def proc = gruntScript.execute()  // execute default task to load dependencies from local cache.
+    proc.waitFor()
+    if (proc.exitValue() != 0) {
+        println "| Error occured while loading dependencies from local cache : ${proc.err.text}"
+        println "| Try loading dependencies from web..."
+        proc = gruntScript.execute()
+        proc.waitFor()                               // Wait for the command to finish
+        println "Output: ${proc.in.text}"
+    }
+}
